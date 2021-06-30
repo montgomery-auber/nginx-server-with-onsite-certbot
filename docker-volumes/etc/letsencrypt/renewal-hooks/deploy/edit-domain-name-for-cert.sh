0acad84a33cc39dea69b4a7eb0cf1192d2da5b59
Montgomery Auber <support@floatingcloud.io>
2021-06-30 19:49:36 +0300

    work on sed to change from dummy certs

--- docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
+++ docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's/dummy.float.i.ng/test.float.i.ng/g' ../docker-volumes/etc/nginx/conf.d/default.conf
+sed -i 's/dummy.float.i.ng/test.float.i.ng/g' /etc/nginx/conf.d/default.conf
 curl -X POST --unix-socket /run/docker.sock http://docker/containers/nginx/restart
