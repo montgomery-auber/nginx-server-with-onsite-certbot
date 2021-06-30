cf07c4750ad50bb041df5ee14838817498be247d
Montgomery Auber <support@floatingcloud.io>
2021-06-30 21:48:24 +0300

    use good ol bourne shell

--- docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
+++ docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
@@ -1,3 +1,3 @@
-#!/bin/bash -x
+#!/bin/sh -x
 sed -i 's/dummy.float.i.ng/test.float.i.ng/g' /etc/nginx/conf.d/default.conf
 curl -X POST --unix-socket /run/docker.sock http://docker/containers/nginx/restart
