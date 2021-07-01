2a91d58a765cea350205113221ee8c71ee8524e9
Montgomery Auber <support@floatingcloud.io>
2021-07-01 19:09:27 +0300

    fix sed command

--- docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
+++ docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
-sed -i 's/${FQDN}/test.float.i.ng/g' /etc/nginx/conf.d/default.conf
+sed -i 's/dummy.float.i.ng/${FQDN}/g' /etc/nginx/conf.d/default.conf
 curl -X POST --unix-socket /run/docker.sock http://docker/containers/nginx/restart
